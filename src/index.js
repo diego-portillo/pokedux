2d5db8b2ab2887647a5763e13bb1c20ac003bbdf
Diego <dportillodeveloper@gmail.com>
2023-08-02 21:38:25 -0400

    Clase 15

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import { pokemonsReducer } from './reducers/pokemons';
+import { Provider} from 'react-redux'
+import { 
+  applyMiddleware,
+  compose, 
+  legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk'
+  import { logger } from './middlewares';
+import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const composeAlt = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composedEnhancers = composeAlt(
+    applyMiddleware(thunk, logger)
+)
+
+const store = createStore(
+  pokemonsReducer,
+  composedEnhancers 
+)
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
